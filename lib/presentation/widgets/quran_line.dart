f43120dd06cdcda8c9260ca451d73d36b1e49a2d
alheekmahlib <haozo89@gmail.com>
2025-01-19 00:55:00 -0800

    version: 0.0.5

--- lib/presentation/widgets/quran_line.dart
+++ lib/presentation/widgets/quran_line.dart
@@ -119,6 +119,7 @@ class QuranLine extends StatelessWidget {
                         color: textColor ?? Colors.black,
                         fontSize: 23.55,
                         fontFamily: "hafs",
+                        height: 1.7,
                         package: "quran_library",
                       ),
                     ),
@@ -131,94 +132,5 @@ class QuranLine extends StatelessWidget {
         );
       },
     );
-  }
-
-  Widget textScaleBuild(BuildContext context, var hasBookmark) {
-    return RichText(
-        text: TextSpan(
-      children: quranCtrl.staticPages[pageIndex].ayahs.map((ayah) {
-        quranCtrl.isAyahSelected =
-            quranCtrl.selectedAyahIndexes.contains(ayah.id);
-        final allBookmarks = bookmarks.values.expand((list) => list).toList();
-        // final String lastCharacter =
-        //     ayah.ayah.substring(ayah.ayah.length - 1);
-        return WidgetSpan(
-          child: GestureDetector(
-            onTap: () {
-              if (onPagePress != null) {
-                onPagePress!();
-              }
-              quranCtrl.clearSelection();
-              quranCtrl.state.overlayEntry?.remove();
-              quranCtrl.state.overlayEntry = null;
-            },
-            onLongPressStart: (details) {
-              if (onAyahLongPress != null) {
-                onAyahLongPress!(details, ayah);
-                quranCtrl.toggleAyahSelection(ayah.id);
-                quranCtrl.state.overlayEntry?.remove();
-                quranCtrl.state.overlayEntry = null;
-              } else {
-                final bookmarkId = allBookmarks
-                        .any((bookmark) => bookmark.ayahId == ayah.id)
-                    ? allBookmarks
-                        .firstWhere((bookmark) => bookmark.ayahId == ayah.id)
-                        .id
-                    : null;
-                if (bookmarkId != null) {
-                  BookmarksCtrl.instance.removeBookmark(bookmarkId);
-                } else {
-                  quranCtrl.toggleAyahSelection(ayah.id);
-                  quranCtrl.state.overlayEntry?.remove();
-                  quranCtrl.state.overlayEntry = null;
-
-                  // إنشاء OverlayEntry جديد
-                  final overlay = Overlay.of(context);
-                  final newOverlayEntry = OverlayEntry(
-                    builder: (context) => AyahLongClickDialog(
-                      ayah: ayah,
-                      position: details.globalPosition,
-                    ),
-                  );
-
-                  quranCtrl.state.overlayEntry = newOverlayEntry;
-
-                  // إدخال OverlayEntry في Overlay
-                  overlay.insert(newOverlayEntry);
-                }
-              }
-            },
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(4.0),
-                color: hasBookmark(ayah.surahNumber, ayah.id).value
-                    ? bookmarksColor
-                    : (bookmarksAyahs.contains(ayah.id)
-                        ? Color(allBookmarks
-                                .firstWhere(
-                                  (b) => b.ayahId == ayah.id,
-                                )
-                                .colorCode)
-                            .withValues(alpha: 0.7)
-                        : quranCtrl.isAyahSelected
-                            ? ayahSelectedBackgroundColor ??
-                                const Color(0xffCDAD80).withValues(alpha: 0.25)
-                            : null),
-              ),
-              child: Text(
-                ayah.ayah.replaceAll('\n', ''),
-                style: TextStyle(
-                  color: textColor ?? Colors.black,
-                  fontSize: 23.55 * quranCtrl.state.scaleFactor.value,
-                  fontFamily: "hafs",
-                  package: "quran_library",
-                ),
-              ),
-            ),
-          ),
-        );
-      }).toList(),
-      style: QuranLibrary().hafsStyle,
-    ));
   }
 }

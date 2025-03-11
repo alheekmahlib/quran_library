f98542a8e4b8206415c5ded6761b59fe560b1749
Mohammad699 <sop7ana.allah@gmail.com>
2025-03-11 12:11:08 +0200

    Add theme toggle button to side drawer

--- lib/presentation/pages/quran_library_screen.dart
+++ lib/presentation/pages/quran_library_screen.dart
@@ -17,6 +17,9 @@ class QuranLibraryScreen extends StatelessWidget {
   ///
   /// This constructor is used to create a new instance of the
   /// [QuranLibraryScreen] widget.
+
+  final VoidCallback? onThemeToggle; // Add this new parameter
+
   const QuranLibraryScreen({
     super.key,
     this.appBar,
@@ -49,6 +52,7 @@ class QuranLibraryScreen extends StatelessWidget {
     this.topTitleChild,
     this.useDefaultAppBar = true,
     this.withPageView = true,
+    this.onThemeToggle, // Include in constructor
   });
 
   /// إذا قمت بإضافة شريط التطبيقات هنا فإنه سيحل محل شريط التطبيقات الافتراضية [appBar]
@@ -271,7 +275,11 @@ class QuranLibraryScreen extends StatelessWidget {
                     )
                   : null),
           drawer: appBar == null && useDefaultAppBar
-              ? _DefaultDrawer(languageCode ?? 'ar', isDark)
+              ? _DefaultDrawer(
+                  languageCode ?? 'ar',
+                  isDark,
+                  onThemeToggle: onThemeToggle ?? () {}, // Pass the callback
+                )
               : null,
           body: SafeArea(
             child: withPageView

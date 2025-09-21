10ca70b8fbd5a1f0ff2cd4d3491645d6404c9eba
Hawazen Sameer <53709203+alheekmahlib@users.noreply.github.com>
2025-09-21 14:24:30 -0700

    upgrade Flutter to latest stable version #19

--- android/src/main/java/com/alheekmah/quranPackage/QuranLibraryPlugin.java
+++ android/src/main/java/com/alheekmah/quranPackage/QuranLibraryPlugin.java
@@ -2,7 +2,7 @@ package com.alheekmah.quranPackage;
 
 import androidx.annotation.NonNull;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
-import io.flutter.plugin.common.PluginRegistry;
+
 
 public class QuranLibraryPlugin implements FlutterPlugin {
     @Override
@@ -15,8 +15,5 @@ public class QuranLibraryPlugin implements FlutterPlugin {
         // No implementation needed for manifest merging
     }
 
-    // Maintain compatibility with apps that don't use the v2 embedding
-    public static void registerWith(PluginRegistry.Registrar registrar) {
-        // No implementation needed for manifest merging
-    }
+
 }
